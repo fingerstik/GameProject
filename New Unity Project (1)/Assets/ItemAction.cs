d50de071aba2766d0c0470744fbe2cb5f2387663
fingerstik <gangsj96@gmail.com>
2021-06-01 12:20:29 +0900

    플레이어 이동 / 대시 / 오브젝트 잡기 / 오브젝트 놓기 구현 [가상 조이스틱 및 버튼 두개 구현]

---
+++ New Unity Project (1)/Assets/ItemAction.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemAction : MonoBehaviour
+{
+     public float speed = 1f;
+
+     public GameObject player;
+     public GameObject playerItemPoint;
+     public PlayerController playerLogic;
+     public Animator ani;
+     public GameObject tablePoint;
+
+     private Vector3 forceDirection;
+     public bool IsPlayerEnter;
+     private bool IsTableEnter;
+     private bool IsEnable;
+     private float Timer;
+     private float WaitingTime;
+
+     public void Awake()
+     {
+          player = GameObject.FindGameObjectWithTag("Player");
+          playerItemPoint = GameObject.FindGameObjectWithTag("ItemPoint");
+
+          ani = player.GetComponent<Animator>();
+          playerLogic = player.GetComponent<PlayerController>();
+          tablePoint = GameObject.FindGameObjectWithTag("Table");
+
+          IsEnable = false;
+
+          Timer = 0.0f;
+          WaitingTime = 0.5f;
+     }
+
+     public void Update()
+     {
+          Timer += Time.deltaTime;
+          if (Timer > WaitingTime) {
+               Timer = 0;
+               if (IsEnable && IsPlayerEnter)
+               {
+                    transform.SetParent(playerItemPoint.transform);
+                    transform.localPosition = Vector3.zero;
+                    transform.rotation = new Quaternion(0, 0, 0, 0);
+
+                    IsPlayerEnter = false;
+               }
+               if (IsEnable && IsTableEnter)
+               {
+                    transform.SetParent(tablePoint.transform);
+                    transform.localScale = new Vector3(0.15f, 0.36f, 0.15f);
+                    transform.localPosition = Vector3.zero;
+                    transform.rotation = new Quaternion(0, 0, 0, 0);
+
+                    IsTableEnter = false;
+               }
+               IsEnable = false;
+          }
+     }
+
+     public void OnEnableDown()
+     {
+          if (!IsEnable)
+          {
+               IsEnable = true;
+               ani.SetTrigger("Enable");
+               
+          }
+     }
+
+
+     private void OnTriggerEnter(Collider other)
+     {
+          if (other.gameObject == player)
+          {
+               IsPlayerEnter = true;
+          }
+          if (other.gameObject == tablePoint)
+          {
+               IsTableEnter = true;
+          }
+     }
+     private void OnTriggerExit(Collider other)
+     {
+          if (other.gameObject == player)
+          {
+               IsPlayerEnter = false;
+          }
+          if (other.gameObject == tablePoint)
+          {
+               IsTableEnter = true;
+          }
+     }
+}
---
+++ New Unity Project (1)/Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+     [Header("Plyaer Condition")]
+     public GameObject PlayerModel;
+     public GameObject PlayerItemPoint;
+     public bool StopByStage;
+     private bool IsMotion;
+     private bool IsDash;
+
+     [Header("Player Moving")]
+     public float speed;
+     public float dashSpeed;
+     private float DashTime;
+     private float DashCoolTime;
+     private bool IsFirstDash;
+     public VariableJoystick variableJoystick;
+
+     [Header("Player Having")]
+
+     [Header("Plyaer Animation")]
+     public Animator ani;
+
+     private Rigidbody playerRB;
+
+     public void Awake()
+     {
+          playerRB = GetComponent<Rigidbody>();
+          ani = GetComponent<Animator>();
+          IsMotion = false;
+          DashCoolTime = Time.time;
+     }
+
+     public void OnDashDown()
+     {
+          if (Time.time - DashCoolTime > 3.0f)
+          {
+               IsDash = true;
+               IsFirstDash = false;
+               ani.SetBool("isDashing", true);
+               DashTime = Time.time;
+               DashCoolTime = Time.time;
+          }
+     }
+     public void OnDashUp()
+     {
+          IsDash = false;
+          ani.SetBool("isDashing", false);
+     }
+
+     public void Update()
+     {
+          //스테이지에 의해 행동 불능
+          if (StopByStage)
+          {
+               ani.SetBool("isMoving", false);
+               return;
+          }
+          Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+          if (!IsMotion)
+          {
+               direction = direction.normalized * speed * Time.fixedDeltaTime;
+               if (direction.x == 0.0f && direction.z == 0.0f) ani.SetBool("isMoving", false);
+               else ani.SetBool("isMoving", true);
+               playerRB.MovePosition(transform.position + direction);
+               if(direction.x != 0.0f || direction.y != 0.0f)
+                    playerRB.transform.rotation = Quaternion.LookRotation(direction);
+               if(Time.time - DashTime < 1.0f && IsDash)
+               {
+                    playerRB.MovePosition(transform.position + direction*dashSpeed);
+                    if (direction.x != 0.0f || direction.y != 0.0f)
+                         playerRB.transform.rotation = Quaternion.LookRotation(direction);
+               }
+               else OnDashUp();
+          }
+     }
+}
