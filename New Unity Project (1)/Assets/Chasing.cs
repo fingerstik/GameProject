64c50094d398c8c639d19e5074e60490cded4549
fingerstik <gangsj96@gmail.com>
2021-06-07 00:03:44 +0900

    start scene 복구 완료

--- New Unity Project (1)/Assets/Chasing.cs
+++ New Unity Project (1)/Assets/Chasing.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Chasing : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+     public GameObject Player;
+     private GameObject Target;
+     public float CameraX;
+     public float CameraY;
+     public float CameraZ;
+     private void Start()
+     {
+          Target = Player;
+          Vector3 TargetPos = new Vector3(Target.transform.position.x + CameraX, Target.transform.position.y + CameraY, Target.transform.position.z + CameraZ);
+          transform.position = TargetPos;
+     }
+     void FixedUpdate()
+     {
+          Vector3 TargetPos = new Vector3(Target.transform.position.x + CameraX, Target.transform.position.y + CameraY, Target.transform.position.z + CameraZ);
+          transform.position = Vector3.Lerp(transform.position, TargetPos, moveRateTime(Time.deltaTime * 2f, 0.5f));
+     }
+     float moveRateTime(float time, float rate)
+     {
+          if (time <= 1)
+               return time / 2;
+          else
+               return 0.5f;
+     }
 }
