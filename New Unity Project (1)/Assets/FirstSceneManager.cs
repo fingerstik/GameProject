73da0441d91836bd1dfb35df24e58ed72cb2ccb8
fingerstik <gangsj96@gmail.com>
2021-06-08 00:07:03 +0900

    현재까지 진행상황[스크립트 오류 해결중]

---
+++ New Unity Project (1)/Assets/FirstSceneManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FirstSceneManager : MonoBehaviour
+{
+     private bool OnReady;
+     // Start is called before the first frame update
+     void Start()
+     {
+          Screen.SetResolution(1920, 1080, false);
+     }
+
+     // Update is called once per frame
+     public void goGameScene()
+     {
+          SceneManager.LoadScene("GameScene");
+     }
+}
---
+++ New Unity Project (1)/Assets/StageManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+     public string TxtMode;   // GUITEXT
+     public string TxtStage;  // GUITEXT
+     public string TxtTime;   // GUITEXT
+     public string TxtTouch;  // GUITEXT
+     public string TxtBonus;  // GUITEXT
+     public string TxtScore;  // GUITEXT
+     public string TxtMsg;    // GUITEXT
+     
+     static STATE State;
+
+     private float StageTime;
+     private int StageScore;
+
+     enum STATE {START, STAGE, END}
+
+     // Start is called before the first frame update
+     void Start()
+     {
+          StageTime = 0;
+          State = STATE.START;
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+          switch (State)
+          {
+               case STATE.START:
+                    StartStage();
+                    break;
+               case STATE.STAGE:
+                    GameStage();
+                    break;
+               case STATE.END:
+                    EndStage();
+
+          }
+     }
+
+     public void StartStage()
+     {
+
+     }
+     public void GameStage()
+     {
+
+     }
+     public void EndStage()
+     {
+
+     }
+}
