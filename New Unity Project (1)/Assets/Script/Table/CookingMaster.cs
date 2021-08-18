0201938fe5dec02a1db1209396fde3166adff2d1
fingerstik <gangsj96@gmail.com>
2021-08-18 17:00:27 +0900

    end proj_6 // end proj_1 to end proj_6 // 프로젝트 정리과정에서 나눠서 커밋

---
+++ New Unity Project (1)/Assets/Script/Table/CookingMaster.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookingMaster : MonoBehaviour
+{
+     GameObject ItemPoint;
+     GameObject player;
+     public GameObject SlicedSalary;
+     public GameObject SlicedTomato;
+     public GameObject RawFish;
+     public GameObject SlicedShrimp;
+     public GameObject TablePoint;
+
+     public bool isPlayerEnter;
+     public bool isButtonDown;
+
+     // Start is called before the first frame update
+     void Start()
+     {
+          player = GameObject.FindGameObjectWithTag("Player");
+          ItemPoint = GameObject.FindGameObjectWithTag("ItemPoint");
+          isPlayerEnter = false;
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+          if (isPlayerEnter && isButtonDown)
+          {
+               if (ItemPoint.transform.childCount != 0 && TablePoint.transform.childCount == 0)
+               {
+                    MakeItem();
+               }
+               isButtonDown = false;
+               isPlayerEnter = false;
+          }
+     }
+     void MakeItem()
+     {
+          GameObject ItemPtr = ItemPoint.GetComponentInChildren<Rigidbody>().gameObject;
+          if (ItemPtr.tag == "Tomato")
+          {
+               Destroy(ItemPtr);
+               Quaternion newRot = Quaternion.Euler(ItemPoint.transform.rotation.x, ItemPoint.transform.rotation.y + 90f, ItemPoint.transform.rotation.z);
+               GameObject MadeItem = Instantiate(SlicedTomato, ItemPoint.transform.position, newRot);
+               MadeItem.transform.SetParent(ItemPoint.transform);
+          }
+          else if (ItemPtr.tag == "Salary")
+          {
+               Destroy(ItemPtr);
+               Quaternion newRot = Quaternion.Euler(ItemPoint.transform.rotation.x, ItemPoint.transform.rotation.y + 90f, ItemPoint.transform.rotation.z);
+               GameObject MadeItem = Instantiate(SlicedSalary, ItemPoint.transform.position, newRot);
+               MadeItem.transform.SetParent(ItemPoint.transform);
+          }
+          else if (ItemPtr.tag == "Fish")
+          {
+               Destroy(ItemPtr);
+               Quaternion newRot = Quaternion.Euler(ItemPoint.transform.rotation.x, ItemPoint.transform.rotation.y + 90f, ItemPoint.transform.rotation.z);
+               GameObject MadeItem = Instantiate(RawFish, ItemPoint.transform.position, newRot);
+               MadeItem.transform.SetParent(ItemPoint.transform);
+          }
+          else if (ItemPtr.tag == "Shrimp")
+          {
+               Destroy(ItemPtr);
+               Quaternion newRot = Quaternion.Euler(ItemPoint.transform.rotation.x, ItemPoint.transform.rotation.y + 90f, ItemPoint.transform.rotation.z);
+               GameObject MadeItem = Instantiate(SlicedShrimp, ItemPoint.transform.position, newRot);
+               MadeItem.transform.SetParent(ItemPoint.transform);
+          }
+          ItemPtr.transform.position = ItemPoint.transform.position;
+          ItemPtr.transform.SetParent(ItemPoint.transform);
+     }
+
+     public void OnButtonDown()
+     {
+          if (isPlayerEnter)
+          {
+               isButtonDown = true;
+          }
+     }
+
+     private void OnTriggerEnter(Collider other)
+     {
+          if (other.gameObject == player)
+          {
+               isPlayerEnter = true;
+          }
+     }
+     private void OnTriggerExit(Collider other)
+     {
+          if (other.gameObject == player)
+          {
+               isPlayerEnter = false;
+          }
+     }
+}
---
+++ New Unity Project (1)/Assets/Script/UI/TimeLeft.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeLeft : MonoBehaviour
+{
+     public float TimeInput;
+     public static float time;
+     public bool IsStop;
+     // Start is called before the first frame update
+     void Start()
+     {
+          time = TimeInput;
+          IsStop = false;
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+          if (!IsStop)
+          {
+               if (time != 0)
+               {
+                    time -= Time.deltaTime;
+                    if (time <= 0)
+                    {
+                         time = 0;
+                    }
+               }
+
+               int t = Mathf.FloorToInt(time);
+               Text uiText = GetComponent<Text>();
+               uiText.text = "Time : " + (t / 60).ToString() + ":" + (t % 60).ToString();
+          }
+
+
+     }
+     public int GetTime()
+     {
+          return (int)time;
+     }
+}
